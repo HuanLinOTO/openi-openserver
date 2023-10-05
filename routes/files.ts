3a254caa680a12c1d76bd2eb71606c1556505aa7
HuanLinOTO <linsmc@126.com>
2023-10-05 21:41:24 +0800

    fix: empty repo

--- routes/files.ts
+++ routes/files.ts
@@ -6,6 +6,11 @@ export default eventHandler(async () => {
   let files: Record<string,any> = {}
   for (const repo in repos_info) {
     const repo_info = repos_info[repo]
+    if(repo_info.count <=0) {
+      console.log("[WARN] No files in repo", repo);
+      
+      continue;
+    }
     files[repo] = repo_info.data[0].attachments.map(item => ({
       name: item.name,
       uuid: item.uuid,
